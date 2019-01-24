1a66fca283cf3567c7b96dfb879c856eac7b4cad
Jy-Liu <junyouliu9@gmail.com>
2019-01-24 19:38:16 +0100

    lint & format

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
 window.onload= function() {
-	//We instantiate our model
-	const model = new DinnerModel();
-	
+  // We instantiate our model
+  const model = new DinnerModel();
+
   // And create the instance of ExampleView
-  //const exampleView = new ExampleView(document.querySelector("#exampleView"));
+  // const exampleView = new ExampleView(document.querySelector("#exampleView"));
 
 
   let welcomeViewContainer = {};
@@ -12,18 +12,18 @@ window.onload= function() {
   welcomeViewContainer.text = document.querySelector("#welcomeText");
   welcomeViewContainer.button = document.querySelector("#startButton");
 
-  // set a if null condition 
+  // set a if null condition
 
   if (Object.values(welcomeViewContainer).find(e => e == undefined) === undefined) {
     const welcomeView = new WelcomeView(welcomeViewContainer, model);
   }
 
-    
-	/**
-	 * IMPORTANT: app.js is the only place where you are allowed to
-	 * query for elements in the whole document.
-	 * In other places you should limit the search only to the children 
-	 * of the specific view you're working with (see exampleView.js).
-	 */
+
+  /**
+   * IMPORTANT: app.js is the only place where you are allowed to
+   * query for elements in the whole document.
+   * In other places you should limit the search only to the children
+   * of the specific view you're working with (see exampleView.js).
+   */
 
 };
