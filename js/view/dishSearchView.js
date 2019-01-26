3b43afdb7cb4c1f48f879ac3b375ccdaee226a68
joarse <joarse@kth.se>
2019-01-26 19:26:21 +0100

    added ES6 dishSearchView.js

---
+++ js/view/dishSearchView.js
@@ -0,0 +1,27 @@
+
+
+class DishSearchView {
+
+  constructor(containers, model) {
+    this.containers = containers.container;
+    this.header = containers.header; 
+    this.inputText = containers.inputText;
+    this.select = containers.select;
+    this.dishTypes = model.dishTypes; // make a const arr in model
+
+    this.header.innerHTML = "Find a dish";
+    this.inputText.defaultValue = "Enter key words";
+
+    this.dishTypes.forEach((type) => {
+      this.select.appendChild(this.createSelectDom);
+    });
+  }
+
+  createSelectDom(type) {
+    // method that returns the options for the select dom
+    let option = document.createElement("option");
+    option.value = type;
+    option.text = type;
+    return option;
+  }
+}
