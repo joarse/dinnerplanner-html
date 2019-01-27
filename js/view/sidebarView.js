5fd3e76d0dcdbf9c2218540455fefffe4ef6381b
joarse <joarse@kth.se>
2019-01-27 11:25:34 +0100

    changed method to update()

--- js/view/sidebarView.js
+++ js/view/sidebarView.js
@@ -7,9 +7,11 @@ class SidearView {
     this.numberOfGuests = containers.numberOfGuests;
     this.menuCost = containers.menuCost;
 
+    this.update();
+  }
+
+  update() {
     this.numberOfGuests.innerHTML = model.getNumberOfGuests();
     this.menuCost.innerHTML = model.getTotalMenuPrice();
   }
-
-
 }
