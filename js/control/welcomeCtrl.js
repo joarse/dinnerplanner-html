612a714b12c1fdd8e040779835e3e36cfdc2bb3f
joarse <joarse@kth.se>
2019-01-30 15:16:04 +0100

    added eventlistener

--- js/control/welcomeCtrl.js
+++ js/control/welcomeCtrl.js
@@ -2,9 +2,8 @@ class WelcomeCtrl {
 
   constructor(welcomeView) {
     this.welcomeView = welcomeView;
+    this.welcomeView.container.find("#startButton").click(event => {
+      // should notify the general state controller that welcomeview is hidden
+      // and the next screen should be shown
+    });
   }
-
-  init() {
-    this.welcomeView.update();
-  }
-}
