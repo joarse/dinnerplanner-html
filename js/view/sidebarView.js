39bdde464d3f819e33247758cbf3617c12ad8b85
joarse <joarse@kth.se>
2019-01-30 11:13:32 +0100

    Added methods for data display

--- js/view/sidebarView.js
+++ js/view/sidebarView.js
@@ -1,17 +1,37 @@
 
 
-class SidearView {
+class SideBarView {
 
-  constructor(containers, model) {
-
-    this.numberOfGuests = containers.numberOfGuests;
-    this.menuCost = containers.menuCost;
-
-    this.update();
+  constructor(container, model) {
+    this.container = container;
+    this.model = model;
+    model.addObserver(this);
+    this.init();
   }
 
-  update() {
-    this.numberOfGuests.innerHTML = model.getNumberOfGuests();
-    this.menuCost.innerHTML = model.getTotalMenuPrice();
+  init() {
+    this.container.find("#guestInput").val(this.model.getNumberOfGuests()); 
+    this.container.find("#menuCost").html(this.model.getTotalMenuPrice() + " SEK");
+  }
+  update(args) {
+    switch (args) {
+      case "numberOFGuests":
+        this.init();
+        break;
+      case "menu":
+        this.container.find("#appendDishes");
+        // add a container and a span with correct information
+
+        this.init();
+        break;
+    }
+  }
+  hide() {
+    this.container.hide();
+  }
+
+  show() {
+    this.container.show();
+    this.init();
   }
 }
