fa675a853303c41b77e656163da3fd9b3a6a2ab9
joarse <joarse@kth.se>
2019-01-30 15:36:20 +0100

    removed redundant method

--- js/control/welcomeCtrl.js
+++ js/control/welcomeCtrl.js
@@ -4,6 +4,7 @@ class WelcomeCtrl {
     this.welcomeView = welcomeView;
     this.welcomeView.container.find("#startButton").click(event => {
       // should notify the general state controller that welcomeview is hidden
-      // and the next screen should be shown
+      // and the next scre
     });
   }
+}
