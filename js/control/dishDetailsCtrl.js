405b24a40dc5341ad3771ebafcb8fd8a33dab365
joarse <joarse@kth.se>
2019-01-31 10:00:45 +0100

    added routing

--- js/control/dishDetailsCtrl.js
+++ js/control/dishDetailsCtrl.js
@@ -1,7 +1,14 @@
 
 class DishDetailsCtrl {
 
-  constructor(dishFinishedView) {
-    this.dishFinishedView = dishFinishedView;
+  constructor(dishDetailsView, model, generalController) {
+    dishDetailsView.container.find("#addToMenuBtn").click(event => {
+      model.addDishToMenu(201); // should get the proper id
+      generalController.showScreen("select_dish");
+    });
+
+    dishDetailsView.container.find("#backToSearchBtn").click(event => {
+      generalController.showScreen("select_dish");
+    });
   }
 }
