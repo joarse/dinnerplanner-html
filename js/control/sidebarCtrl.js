3415591e49a73c36f77be0d40e55b184c1694d53
joarse <joarse@kth.se>
2019-01-31 08:56:48 +0100

    update btn and routing

--- js/control/sidebarCtrl.js
+++ js/control/sidebarCtrl.js
@@ -1,12 +1,18 @@
 
 class SideBarCtrl {
 
-  constructor(sidebarView, model) {
+  constructor(sidebarView, model, generalController) {
     this.sidebarView = sidebarView;
     this.model = model;
 
     this.sidebarView.container.find("#guestInput").change(event => {
       this.model.setNumberOfGuests(event.target.value);
+    });
+
+    this.sidebarView.container.find("#sidebarBtn").click(event => {
+      // should notify the general state controller that welcomeview is hidden
+      // and the next scre
+      generalController.showScreen("dinner_overview");
     });
   }
 }
