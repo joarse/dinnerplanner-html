a41fd29d33b010fbf0d88b4b68213ad24ea92294
joarse <joarse@kth.se>
2019-02-03 20:40:08 +0100

    added dynamic itemView to display menu

--- js/view/dishFinishedView.js
+++ js/view/dishFinishedView.js
@@ -3,19 +3,21 @@ class DishFinishedView {
 
   constructor(container, model) {
     this.container = container;
+    this.itemContainer = this.container.find("#finishedItemDiv");
     this.model = model;
     this.model.addObserver(this);
-    /*
-    this.header = containers.header;
-    this.editBtn = containers.editBtn;
-    this.printBtn = containers.printBtn;
-
-    this.header.innerHTML = "My Dinner: " + model.getNumberOfGuests() + " people";
-    this.editBtn.innerHTML = "Go back and edit dinner";
-    this.printBtn.innerHTML = "PrintFullRecipe";
-    */
   }
   init() {
+    this.itemContainer.empty();
+    this.model.getFullMenu().forEach((dish) => {
+     
+    // create dish item
+      let itemView = new ItemView(dish.id, dish.image, dish.name)
+      this.itemContainer.append(itemView.container);
+    // create dish item controller and bind event on it
+      let itemCrtl = new ItemCtrl(itemView, this.model, this.generalController);
+      itemCrtl.bind();
+      });
     this.container.find("#printButton").html("Print full recipe");
     this.container.find("dishTotalPrice").html(this.model.getTotalMenuPrice());
   }
