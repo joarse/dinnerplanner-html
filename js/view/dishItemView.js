19baffa75a8c42a36119374d23819be34ce51954
joarse <joarse@kth.se>
2019-02-05 19:19:11 +0100

    added semicolon

--- js/view/dishItemView.js
+++ js/view/dishItemView.js
@@ -11,7 +11,7 @@ class DishItemView {
     // show all dish item
     this.model.getAllDishes().forEach((dish) => {
       // create dish item
-      let itemView = new ItemView(dish.id, dish.image, dish.name)
+      let itemView = new ItemView(dish.id, dish.image, dish.name);
       this.container.append(itemView.container);
       // create dish item controller and bind event on it
       let itemCrtl = new ItemCtrl(itemView, this.model, this.generalController);
