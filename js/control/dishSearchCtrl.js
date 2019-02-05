36409130af6a3fbc3acf6057187fb6f4bbdc0953
joarse <joarse@kth.se>
2019-02-05 20:04:34 +0100

    added event listeners

--- js/control/dishSearchCtrl.js
+++ js/control/dishSearchCtrl.js
@@ -1,7 +1,21 @@
 
 class DishSearchCtrl {
 
-  constructor(dishSearchView) {
+  constructor(dishSearchView, model, generalController) {
     this.dishSearchView = dishSearchView;
+    this.model = model;
+    this.generalController = generalController;
+
+    this.dishSearchView.container.find("#inputText").change(event => {
+      console.log(this.dishSearchView.container.find("#inputText")[0].value);
+    });
+
+    this.dishSearchView.container.find("#inputSelect").change(event => {
+      console.log(this.dishSearchView.container.find("#inputSelect").val());
+    });
+
+    this.dishSearchView.container.find("#searchButton").click(event => {
+      console.log("Button clicked");
+    });
   }
 }
