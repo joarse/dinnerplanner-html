d31126d3bb8df70eccac66840e6e54ee8606a16b
joarse <joarse@kth.se>
2019-02-06 18:47:55 +0100

    thumbnail class for image

--- js/view/ItemView.js
+++ js/view/ItemView.js
@@ -7,7 +7,7 @@ class ItemView {
   createItem(id, image, text) {
     let div = `
     <div id="${id}" class="col-sm-6 col-md-4 col-lg-3 dish">
-      <img src="${image}"/>
+      <img src="${image}" class ="img-thumbnail"/>
       <label>${text}</label>
     </div>`;
     return $(div);
