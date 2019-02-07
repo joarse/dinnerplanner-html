189f995c60e2c704e7eb63b4269d8110324a30d5
Jy-Liu <junyouliu9@gmail.com>
2019-02-07 15:18:20 +0100

    add cache

--- js/control/ItemCtrl.js
+++ js/control/ItemCtrl.js
@@ -12,21 +12,27 @@ class ItemCtrl {
       this.model.selectedDishItem = id;
 
       // get detailed info of one dish
-      // TODO: getDish from model if it exists
-      // TODO: add spinner
-      this.model.getDetailedInfo(id)
-      .then(data => {
-        this.model.parseDish(data);
-      })
-      .catch(e => {
-        alert("There's something wrong in fetching dish!!!");
-        // TODO: redirect
-        console.log(e)
-      })
-      .then(res => {
-        // TODO: rm spinner
+      // getDish from model if it exists
+      if(Object.keys(this.model.dishes).includes(id)) {
+        console.log("same");
         this.generalController.confirmState("CLICK_DISH");
-      });
+      }
+      else {
+        // TODO: add spinner
+        this.model.getDetailedInfo(id)
+        .then(data => {
+          this.model.parseDish(data);
+        })
+        .catch(e => {
+          alert("There's something wrong in fetching dish!!!");
+          // TODO: redirect
+          console.log(e)
+        })
+        .then(res => {
+          // TODO: rm spinner
+          this.generalController.confirmState("CLICK_DISH");
+        });
+      }
     });
   }
 }
